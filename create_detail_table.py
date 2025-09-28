3c85ff00361fc032da9bb5c25977e10537da2356
abhishek2128 <abhishekyadav2171997@gmail.com>
2025-09-28 22:36:51 +0530

    Update create_detail_table.py

--- create_detail_table.py
+++ create_detail_table.py
@@ -15,6 +15,7 @@ ship_type_data=pd.read_csv('/home/abhishekyadav/stig_project/src/data/ship_type_
 
 if 'LR No' in sers_fleet_share_data.columns:
     sers_fleet_share_data = sers_fleet_share_data.rename(columns={'LR No': 'IMO No'})
+
 if 'LRNo' in auto_mobile_data.columns:
     auto_mobile_data = auto_mobile_data.rename(columns={'LRNo': 'IMO No'})
 
@@ -31,19 +32,21 @@ columns2 = set(sers_fleet_share_data.columns)
 # {'LR No', 'SERS Comments', 'Status', 'Ship Name', 'Contract Signed Date', 'Client Name', 
 #  'Project Type', 'Country', 'Client Report', 'Documents', 'Last Updated', 'Annual Sub Month'}
 
-# Find common columns
-common_columns = columns1.intersection(columns2)
-get_region = pd.merge(sea_web_data, regions_data, on='Technical Manager Country of Domicile', how='inner')
-combined = pd.merge(sers_fleet_share_data, sea_web_data, on='IMO No', how='inner')
+# Merge SEA data with region info by country
+get_region = pd.merge(sea_web_data, regions_data, on='Technical Manager Country of Domicile', how='inner')  #Comman Technical Manager Country of Domicile
+
+# Merge fleet share data with SEA data by IMO No
+combined = pd.merge(sers_fleet_share_data, sea_web_data, on='IMO No', how='inner') # comman IMO NO
 
 combined['IMO No'] = combined['IMO No'].astype(str)
 case_summary_data['IMO No'] = case_summary_data['IMO No'].astype(str)
 get_region['IMO No'] = get_region['IMO No'].astype(str)
 auto_mobile_data['IMO No'] = auto_mobile_data['IMO No'].astype(str)
 
-# Now merge
+# Merge case summary data by IMO No
 combined = pd.merge(combined, case_summary_data, on='IMO No', how='inner')
 
+# Merge region info into combined data by IMO No
 combined=pd.merge(combined, get_region, on='IMO No', how='inner')
 combined.columns= [col.replace('_x', '').replace('_y', '') for col in combined.columns]
 
@@ -53,6 +56,7 @@ combined = pd.merge(combined, auto_mobile_data, on='IMO No', how='left')
 
 # Create a new column to indicate "Yes" or "No" based on the match
 combined['transfer'] = combined['IMO No'].apply(lambda x: 'Y' if x == 'both' else 'N')
+
 valid_imo_numbers = set(combined['IMO No'])
 # Step 3: Check if 'IMO No' from file1 exists in the set of valid IMO numbers
 combined['sers_sister'] = combined['IMO No'].apply(lambda x: 'Y' if x in valid_imo_numbers else 'N')
@@ -66,13 +70,14 @@ combined = pd.merge(combined, ship_type_data, on='Type detail', how='inner')
 
 ################################---------------------------------###################
 
-#####_____________________________________############
+
 
 sea_web_data['IMO Count'] = sea_web_data['GLIMO'].map(sea_web_data['GLIMO'].value_counts())
 def assign_role_group(group):
     count = len(group)
     if count == 1:
         group['Client lead / sister'] = 'Lead'
+
     elif count == 2:
         earliest_dob = group['DOB'].min()
         group['Client lead / sister'] = group['DOB'].apply(lambda x: 'Lead' if x == earliest_dob else 'Sister')
@@ -87,7 +92,7 @@ client_ls['IMO No']=client_ls['IMO No'].astype(str)
 
 combined=pd.merge(combined, client_ls, on='IMO No', how='left')
 
-#################-------------------Enrol type-----------------#####################
+#-------------------Enrol type-----------------
 def get_enrol_type(row):
     # Step 1: Check if AB has a value
     if pd.notnull(row['Status']) and row['Status'] != '':
@@ -113,6 +118,7 @@ fee_mapping = {
     'Lead': 5000,
     'Sister': 1000,
     'Passenger Ship': 10300,
+    'Passenger':10300,
     'Ferry': 10300,
     'Container': 7700,
     'Tanker': 5750,
@@ -122,8 +128,10 @@ fee_mapping = {
     'Ro/Ro Cargo': 7700,
     'Roro cargo':7700,
     'vehicle carrier': 7700,
+    'Vehicle carrier':7700,
     'gas':7050,
     'General Cargo'	:7050,
+    'General cargo':7050,
     'OSV': 7050,
     'Yacht'	:9500,
     'Other'	:7050,
@@ -159,5 +167,5 @@ select_col=['Technical Manager Name', 'Technical Manager Country of Domicile', '
               'ECO NOTATION FLAG', 'SSD', 'GLIMO', 'Contract Signed Date', 'Status' ,
               'transfer', 'sers_sister', 'Client lead / sister', 'Enrol type', 'Enrol Fee']
 final_data=combined[select_col]
-final_data.to_csv('stig_detail_table_26.csv', index=False)
+final_data.to_csv('stig_detail_table_29.csv', index=False)
 
